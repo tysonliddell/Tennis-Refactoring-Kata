099c688b38900d8ffbc4e42975ab36a49624122a
Tyson Liddell <tysonliddell@hotmail.com>
2023-10-18 14:48:33 +0100

    Tennis 3: First refactoring.

--- python/tennis/tennis3.py
+++ python/tennis/tennis3.py
@@ -1,30 +1,64 @@
 # -*- coding: utf-8 -*-
 
+from dataclasses import dataclass
+
+PLAYER1_NAME = "player1"
+BEFORE_DEUCE_SCORE_NAMES = ["Love", "Fifteen", "Thirty", "Forty"]
+
+
+@dataclass
+class Player:
+    name: str
+    score: int
+
 
 class TennisGame3:
-    def __init__(self, player1Name, player2Name):
-        self.p1N = player1Name
-        self.p2N = player2Name
-        self.p1 = 0
-        self.p2 = 0
+    def __init__(self, player1Name: str, player2Name: str):
+        self.player1 = Player(player1Name, 0)
+        self.player2 = Player(player2Name, 0)
 
-    def won_point(self, n):
-        if n == "player1":
-            self.p1 += 1
+    def won_point(self, player_name: str):
+        if player_name == PLAYER1_NAME:
+            self.player1.score += 1
         else:
-            self.p2 += 1
+            self.player2.score += 1
 
-    def score(self):
-        if (self.p1 < 4 and self.p2 < 4) and (self.p1 + self.p2 < 6):
-            p = ["Love", "Fifteen", "Thirty", "Forty"]
-            s = p[self.p1]
-            return s + "-All" if (self.p1 == self.p2) else s + "-" + p[self.p2]
+    def has_deuce_been_scored(self) -> bool:
+        return self.player1.score + self.player2.score >= 6
+
+    def winning_player(self) -> Player:
+        if self.player1.score > self.player2.score:
+            return self.player1
         else:
-            if self.p1 == self.p2:
-                return "Deuce"
-            s = self.p1N if self.p1 > self.p2 else self.p2N
+            return self.player2
+
+    def score(self) -> str:
+        if self.has_deuce_been_scored():
+            return self._after_deuce_score()
+        else:
+            return self._before_deuce_score()
+
+    def _before_deuce_score(self) -> str:
+        """Get the score, when deuce has been scored."""
+        winning_player = self.winning_player()
+        if winning_player.score == 4:
+            return "Win for " + winning_player.name
+        else:
+            named_score = BEFORE_DEUCE_SCORE_NAMES[self.player1.score]
             return (
-                "Advantage " + s
-                if ((self.p1 - self.p2) * (self.p1 - self.p2) == 1)
-                else "Win for " + s
+                named_score + "-All"
+                if (self.player1.score == self.player2.score)
+                else named_score + "-" + BEFORE_DEUCE_SCORE_NAMES[self.player2.score]
+            )
+
+    def _after_deuce_score(self) -> str:
+        """Get the score, when deuce has not been scored."""
+        winning_player = self.winning_player()
+        if self.player1.score == self.player2.score:
+            return "Deuce"  # 40-All
+        else:
+            return (
+                "Advantage " + winning_player.name
+                if abs(self.player1.score - self.player2.score) == 1
+                else "Win for " + winning_player.name
             )
