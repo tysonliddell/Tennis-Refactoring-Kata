c7a1d3af6de5aa0adb6ef0366bdb9bfa94a25a91
Tyson Liddell <tysonliddell@hotmail.com>
2023-10-19 20:15:52 +0100

    Tennis 1 refactoring.

--- python/tennis/tennis1.py
+++ python/tennis/tennis1.py
@@ -1,49 +1,57 @@
 # -*- coding: utf-8 -*-
 
+SCORE_FROM_POINTS_VALUE = {
+    0: "Love",
+    1: "Fifteen",
+    2: "Thirty",
+    3: "Forty",
+}
+TIE_SCORE_FROM_POINTS_VALUE = {
+    0: f"{SCORE_FROM_POINTS_VALUE[0]}-All",
+    1: f"{SCORE_FROM_POINTS_VALUE[1]}-All",
+    2: f"{SCORE_FROM_POINTS_VALUE[2]}-All",
+}
+DEUCE_SCORE = "Deuce"
+PLAYER1_ADVANTAGE_SCORE = "Advantage player1"
+PLAYER2_ADVANTAGE_SCORE = "Advantage player2"
+PLAYER1_WIN_SCORE = "Win for player1"
+PLAYER2_WIN_SCORE = "Win for player2"
+
 
 class TennisGame1:
     def __init__(self, player1Name, player2Name):
         self.player1Name = player1Name
         self.player2Name = player2Name
-        self.p1points = 0
-        self.p2points = 0
+        self.player1Points = 0
+        self.player2Points = 0
 
     def won_point(self, playerName):
         if playerName == self.player1Name:
-            self.p1points += 1
+            self.player1Points += 1
         else:
-            self.p2points += 1
+            self.player2Points += 1
+
+    def _is_early_game(self) -> bool:
+        return self.player1Points < 4 and self.player2Points < 4
 
     def score(self):
-        result = ""
-        tempScore = 0
-        if self.p1points == self.p2points:
-            result = {
-                0: "Love-All",
-                1: "Fifteen-All",
-                2: "Thirty-All",
-            }.get(self.p1points, "Deuce")
-        elif self.p1points >= 4 or self.p2points >= 4:
-            minusResult = self.p1points - self.p2points
-            if minusResult == 1:
-                result = "Advantage player1"
-            elif minusResult == -1:
-                result = "Advantage player2"
-            elif minusResult >= 2:
-                result = "Win for player1"
-            else:
-                result = "Win for player2"
+        if self.player1Points == self.player2Points:
+            result = TIE_SCORE_FROM_POINTS_VALUE.get(self.player1Points, DEUCE_SCORE)
+        elif self._is_early_game():
+            result = "-".join(
+                [
+                    SCORE_FROM_POINTS_VALUE[self.player1Points],
+                    SCORE_FROM_POINTS_VALUE[self.player2Points],
+                ]
+            )
         else:
-            for i in range(1, 3):
-                if i == 1:
-                    tempScore = self.p1points
-                else:
-                    result += "-"
-                    tempScore = self.p2points
-                result += {
-                    0: "Love",
-                    1: "Fifteen",
-                    2: "Thirty",
-                    3: "Forty",
-                }[tempScore]
+            minusResult = self.player1Points - self.player2Points
+            if minusResult == 1:
+                result = PLAYER1_ADVANTAGE_SCORE
+            elif minusResult == -1:
+                result = PLAYER2_ADVANTAGE_SCORE
+            elif minusResult >= 2:
+                result = PLAYER1_WIN_SCORE
+            else:
+                result = PLAYER2_WIN_SCORE
         return result
