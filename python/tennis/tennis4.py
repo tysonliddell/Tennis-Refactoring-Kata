3df72ad0fc3763db8ba0c6da17fd270ffdb69397
Tyson Liddell <tysonliddell@hotmail.com>
2023-10-20 20:12:57 +0100

    Tennis 4 refactor.

--- python/tennis/tennis4.py
+++ python/tennis/tennis4.py
@@ -2,6 +2,8 @@
 
 
 class TennisGame4:
+    SCORES = ["Love", "Fifteen", "Thirty", "Forty"]
+
     def __init__(self, player1Name, player2Name):
         self.server = player1Name
         self.receiver = player2Name
@@ -9,23 +11,34 @@ class TennisGame4:
         self.receiverScore = 0
 
     def won_point(self, playerName):
-        if self.server == playerName:
+        if playerName == self.server:
             self.serverScore += 1
         else:
             self.receiverScore += 1
 
     def score(self):
-        result = Deuce(
-            self,
-            GameServer(
-                self,
-                GameReceiver(
-                    self,
-                    AdvantageServer(self, AdvantageReceiver(self, DefaultResult(self))),
-                ),
-            ),
-        ).getResult()
-        return result.format()
+        if self.isTie():
+            if self.isDeuce():
+                result = "Deuce"
+            else:
+                result = TennisGame4.SCORES[self.serverScore] + "-All"
+        elif self.serverHasWon():
+            result = "Win for " + self.server
+        elif self.receiverHasWon():
+            result = "Win for " + self.receiver
+        elif self.serverHasAdvantage():
+            result = "Advantage " + self.server
+        elif self.receiverHasAdvantage():
+            result = "Advantage " + self.receiver
+        else:
+            result = "-".join(
+                [
+                    TennisGame4.SCORES[self.serverScore],
+                    TennisGame4.SCORES[self.receiverScore],
+                ]
+            )
+
+        return result
 
     def receiverHasAdvantage(self):
         return self.receiverScore >= 4 and (self.receiverScore - self.serverScore) == 1
@@ -39,88 +52,8 @@ class TennisGame4:
     def serverHasWon(self):
         return self.serverScore >= 4 and (self.serverScore - self.receiverScore) >= 2
 
+    def isTie(self):
+        return self.serverScore == self.receiverScore
+
     def isDeuce(self):
-        return (
-            self.serverScore >= 3
-            and self.receiverScore >= 3
-            and (self.serverScore == self.receiverScore)
-        )
-
-
-class TennisResult:
-    def __init__(self, serverScore, receiverScore):
-        self.serverScore = serverScore
-        self.receiverScore = receiverScore
-
-    def format(self):
-        if "" == self.receiverScore:
-            return self.serverScore
-        if self.serverScore == self.receiverScore:
-            return self.serverScore + "-All"
-        return self.serverScore + "-" + self.receiverScore
-
-
-class Deuce:
-    def __init__(self, game, nextResult):
-        self.game = game
-        self.nextResult = nextResult
-
-    def getResult(self):
-        if self.game.isDeuce():
-            return TennisResult("Deuce", "")
-        return self.nextResult.getResult()
-
-
-class GameServer:
-    def __init__(self, game, nextResult):
-        self.game = game
-        self.nextResult = nextResult
-
-    def getResult(self):
-        if self.game.serverHasWon():
-            return TennisResult("Win for " + self.game.server, "")
-        return self.nextResult.getResult()
-
-
-class GameReceiver:
-    def __init__(self, game, nextResult):
-        self.game = game
-        self.nextResult = nextResult
-
-    def getResult(self):
-        if self.game.receiverHasWon():
-            return TennisResult("Win for " + self.game.receiver, "")
-        return self.nextResult.getResult()
-
-
-class AdvantageServer:
-    def __init__(self, game, nextResult):
-        self.game = game
-        self.nextResult = nextResult
-
-    def getResult(self):
-        if self.game.serverHasAdvantage():
-            return TennisResult("Advantage " + self.game.server, "")
-        return self.nextResult.getResult()
-
-
-class AdvantageReceiver:
-    def __init__(self, game, nextResult):
-        self.game = game
-        self.nextResult = nextResult
-
-    def getResult(self):
-        if self.game.receiverHasAdvantage():
-            return TennisResult("Advantage " + self.game.receiver, "")
-        return self.nextResult.getResult()
-
-
-class DefaultResult:
-    def __init__(self, game):
-        self.game = game
-        self.scores = ["Love", "Fifteen", "Thirty", "Forty"]
-
-    def getResult(self):
-        return TennisResult(
-            self.scores[self.game.serverScore], self.scores[self.game.receiverScore]
-        )
+        return self.serverScore >= 3 and self.receiverScore >= 3 and self.isTie()
